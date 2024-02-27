a09eb697582094c07e1caf5be6e3667690200299
Sohil0070 <153104236+Sohil0070@users.noreply.github.com>
2024-02-27 13:54:04 +0530

    delete

--- plugins/commands.py
+++ plugins/commands.py
@@ -470,9 +470,9 @@ async def start(client, message):
             ]
         )
     )
-     btn = [[
-         InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
-     ]]
+    btn = [[
+        InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
+    ]]
     k = await msg.reply("<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>10 mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",quote=True)
     await asyncio.sleep(600)
     await msg.delete()
